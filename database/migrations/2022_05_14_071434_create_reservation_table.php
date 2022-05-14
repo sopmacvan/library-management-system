26e55e44d8fa45340fe2a18ec0b5faa9a9173f9e
Reijhx Van <reijhxvan.campos@gmail.com>
2022-05-14 15:16:43 +0800

    added create_reservation_table migration script

---
+++ database/migrations/2022_05_14_071434_create_reservation_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id();
+            $table->integer('book_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->date('reservation_date');
+            $table->integer('reservation_status_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+};
