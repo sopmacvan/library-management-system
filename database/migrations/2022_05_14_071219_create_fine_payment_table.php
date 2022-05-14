78d610eaf9c8b76a8bf7eb18c91d315bb71d3483
Reijhx Van <reijhxvan.campos@gmail.com>
2022-05-14 15:13:58 +0800

    added create_fine_payment_table migration script

---
+++ database/migrations/2022_05_14_071219_create_fine_payment_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fine_payment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->integer('loan_id')->nullable();
+            $table->date('payment_date');
+            $table->integer('payment_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fine_payment');
+    }
+};
